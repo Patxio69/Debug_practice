d8f30bb69f4bc5d61689bff80e3c4d741b708a9d
AlphCode <38281563+Patxio69@users.noreply.github.com>
2019-08-31 09:19:00 +0530

    first GUI project - base

---
+++ GUI/GUI app.py
@@ -0,0 +1,10 @@
+import tkinter as tk 
+from tkinter import ttk
+
+win = tk.Tk()
+win.title("a fucking program")
+
+just_a_lable = ttk.Label(win, text="a fucking shit")
+just_a_lable.grid(row=0, column=0)
+
+def dontclickme(
